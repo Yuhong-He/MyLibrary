e54083ef70de1e59ef1fa798730b39d92e1ad8f2
yuhong <heyuhong1273@gmail.com>
2022-08-24 21:22:21 +0100

    split username display and login button

--- Assets/common/userLogin.js
+++ Assets/common/userLogin.js
@@ -3,11 +3,15 @@ function checkLoginAndDisplayUsername() {
 }
 function displayUsername() {
     const username = getCookie("username");
-    if(username===""){
-        const usernameReplaceLogin = document.getElementById('login_btn');
-        usernameReplaceLogin.innerHTML="登录";
+    if(username===""||username===null){
+        const usernameReplaceLogin = document.getElementById('username_btn');
+        usernameReplaceLogin.innerHTML = "";
+        const usernameBlock = document.getElementById('username_block');
+        usernameBlock.style.display = "none";
     } else {
-        const usernameReplaceLogin = document.getElementById('login_btn');
-        usernameReplaceLogin.innerHTML=username;
+        const usernameReplaceLogin = document.getElementById('username_btn');
+        usernameReplaceLogin.innerHTML = username;
+        const usernameBlock = document.getElementById('username_block');
+        usernameBlock.style.display = "block";
     }
 }
