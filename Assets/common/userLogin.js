d40e3e0190fe302679fe2c0d7dd03a6b8ad53e81
yuhong <heyuhong1273@gmail.com>
2022-08-24 23:24:00 +0100

    add manage block in nav for user with admin authority

--- Assets/common/userLogin.js
+++ Assets/common/userLogin.js
@@ -11,6 +11,8 @@ function userDisplay() {
         usernameBlock.style.display = "none";
         const obtainBlock = document.getElementById('nav_obtain');
         obtainBlock.style.display = "none";
+        const manageBlock = document.getElementById('nav_manage');
+        manageBlock.style.display = "none";
         const loginLogout = document.getElementById('loginLogout_btn');
         loginLogout.innerHTML = "登录";
     } else {
@@ -20,11 +22,21 @@ function userDisplay() {
         usernameBlock.style.display = "block";
         const obtainBlock = document.getElementById('nav_obtain');
         obtainBlock.style.display = "block";
+        const manageBlock = document.getElementById('nav_manage');
+        manageBlock.style.display = "none";
         const loginLogout = document.getElementById('loginLogout_btn');
         loginLogout.innerHTML = "登出";
     }
 }
 
 function adminDisplay() {
-
+    const username = getCookie("username");
+    const auth = getCookie(username + "Auth");
+    if(auth==="2"){
+        const manageBlock = document.getElementById('nav_manage');
+        manageBlock.style.display = "block";
+    } else {
+        const manageBlock = document.getElementById('nav_manage');
+        manageBlock.style.display = "none";
+    }
 }
