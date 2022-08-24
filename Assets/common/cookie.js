3b5d9cd3edf293c80874cb66a1176f4219140f11
yuhong <heyuhong1273@gmail.com>
2022-08-24 22:50:41 +0100

    split cookie function to common js file

---
+++ Assets/common/cookie.js
@@ -0,0 +1,23 @@
+function setCookie(username, password, email) {
+    document.cookie="username=" + username;
+    document.cookie=username + "=" + password;
+    document.cookie=username + "Email=" + email;
+}
+
+function getCookie(cName)
+{
+    const name = cName + "=";
+    const ca = document.cookie.split(';');
+    for(let i=0; i<ca.length; i++)
+    {
+        const c = ca[i].trim();
+        if (c.indexOf(name)===0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+function destroyCookie(username) {
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = username + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = username + "Email=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+}
