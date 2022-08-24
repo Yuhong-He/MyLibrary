ee242828a3d63ee2816a443ff574018e9145aea4
yuhong <heyuhong1273@gmail.com>
2022-08-24 21:56:05 +0100

    combine login and logout button

--- Assets/common/userLogin.js
+++ Assets/common/userLogin.js
@@ -8,10 +8,14 @@ function displayUsername() {
         usernameReplaceLogin.innerHTML = "";
         const usernameBlock = document.getElementById('username_block');
         usernameBlock.style.display = "none";
+        const loginLogout = document.getElementById('loginLogout_btn');
+        loginLogout.innerHTML = "登录";
     } else {
         const usernameReplaceLogin = document.getElementById('username_btn');
         usernameReplaceLogin.innerHTML = username;
         const usernameBlock = document.getElementById('username_block');
         usernameBlock.style.display = "block";
+        const loginLogout = document.getElementById('loginLogout_btn');
+        loginLogout.innerHTML = "登出";
     }
 }
