85d3643617262aced7e2f8f7e71ec5b6e671a68b
yuhong <heyuhong1273@gmail.com>
2022-08-24 22:30:51 +0100

    set active nav block color

--- Assets/common/userLogin.js
+++ Assets/common/userLogin.js
@@ -1,21 +1,30 @@
-function checkLoginAndDisplayUsername() {
-    setTimeout(() => displayUsername(), 50);
+function checkLoginAndDisplay() {
+    setTimeout(() => userDisplay(), 50);
+    setTimeout(() => adminDisplay(), 50);
 }
-function displayUsername() {
+function userDisplay() {
     const username = getCookie("username");
     if(username===""||username===null){
         const usernameReplaceLogin = document.getElementById('username_btn');
         usernameReplaceLogin.innerHTML = "";
-        const usernameBlock = document.getElementById('username_block');
+        const usernameBlock = document.getElementById('nav_profile');
         usernameBlock.style.display = "none";
+        const obtainBlock = document.getElementById('nav_obtain');
+        obtainBlock.style.display = "none";
         const loginLogout = document.getElementById('loginLogout_btn');
         loginLogout.innerHTML = "登录";
     } else {
         const usernameReplaceLogin = document.getElementById('username_btn');
         usernameReplaceLogin.innerHTML = username;
-        const usernameBlock = document.getElementById('username_block');
+        const usernameBlock = document.getElementById('nav_profile');
         usernameBlock.style.display = "block";
+        const obtainBlock = document.getElementById('nav_obtain');
+        obtainBlock.style.display = "block";
         const loginLogout = document.getElementById('loginLogout_btn');
         loginLogout.innerHTML = "登出";
     }
+}
+
+function adminDisplay() {
+
 }
