5a82f00c188ad9651a0c0847cad238b07a5e20c4
yuhong <heyuhong1273@gmail.com>
2022-08-25 17:13:24 +0100

    move php file to common and use local icon

--- Assets/common/php/register.php
+++ Assets/common/php/register.php
@@ -10,7 +10,6 @@ while($row=mysqli_fetch_row($username))
     $all_user_name[]=$row[0];
 }
 $code=202;
-$message="用户名已存在";
 for($start=0; $start<sizeof($all_user_name); $start++)
 {
     if($un==$all_user_name[$start])
@@ -22,14 +21,12 @@ for($start=0; $start<sizeof($all_user_name); $start++)
         $result=mysqli_query($db,$sql);
         if($result==true){
             $code=200;
-            $message="注册成功";
         }
     }
 }
 $str = array
 (
     'code'=>$code,
-    'message'=>$message,
     'email'=>$em,
     'authority'=>1
 );
