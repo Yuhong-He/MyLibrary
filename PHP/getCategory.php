8e21f8f3d24eb0ca3ce1fd299584111c811ec4f6
yuhong <heyuhong1273@gmail.com>
2022-09-20 23:39:59 +0100

    some updates

--- PHP/getCategory.php
+++ PHP/getCategory.php
@@ -3,20 +3,23 @@ header('Content-Type:text/json;charset=utf-8');
 require_once "db.php";
 $lang = $_GET['lang'] ?? '';
 $search = $_GET['search'] ?? '';
-if($search != "") {
-    $search = preg_replace("/[\']/","\\'", $search);
-    if($lang == "zh"){
-        $sql = "SELECT CategoryID, CategoryName FROM category WHERE CategoryName LIKE '%$search%'";
+if(strlen($search) > 0) {
+    if (!preg_match("/[;]/", $search)) {
+        $search = preg_replace("/[\']/", "\\'", $search);
+        if ($lang == "zh") {
+            $sql = "SELECT CategoryID, CategoryName FROM category WHERE CategoryName LIKE '%$search%'";
+        } else {
+            $sql = "SELECT CategoryID, EnCatName FROM category WHERE EnCatName LIKE '%$search%'";
+        }
+        $result = mysqli_query($db, $sql);
+        $arr = [];
+        while ($row = mysqli_fetch_array($result)) {
+            $arr[] = ["id" => $row[0], "name" => $row[1]];
+        }
+        echo json_encode($arr);
     } else {
-        $sql = "SELECT CategoryID, EnCatName FROM category WHERE EnCatName LIKE '%$search%'";
+        echo "Are u trying to do something?\n:(";
     }
-    $result = mysqli_query($db, $sql);
-    $arr = [];
-    while($row = mysqli_fetch_array($result))
-    {
-        $arr[] = ["id"=>$row[0], "name"=>$row[1]];
-    }
-    echo json_encode($arr);
 } else {
     echo "Are u trying to do something?\n:(";
 }
